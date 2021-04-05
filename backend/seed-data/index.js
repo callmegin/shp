ebd1974e0a43acfd34f3aeb5fa06ad0735cc9ecf
Gin <gintautas.anduzis@gmail.com>
2021-04-05 15:54:01 +0300

    read and parse data from json

--- backend/seed-data/index.js
+++ backend/seed-data/index.js
@@ -1,13 +1,18 @@
 const cloudinary = require('cloudinary').v2;
 const mongoose = require('mongoose');
 require('dotenv/config');
+const fs = require('fs');
 const modelSchemas = require('../models');
 
+const sampleData = './seed-data/sampleData.json';
 const dbURL = process.env.DATABASE_URL;
 
 //TODO: update database info with stored cloudinary response in sampleData.json
-
+//TODO: from json: generate products, categories, productimages
 //---------------------------------------------------------------
+//get data from json
+const data = JSON.parse(fs.readFileSync(sampleData, 'utf8'));
+
 const getData = async () => {
   const conn = mongoose.createConnection(dbURL);
   const data = await conn
@@ -20,15 +25,13 @@ const getData = async () => {
   });
 };
 
-const addToDb = async (data) => {
+const addToDb = async () => {
   const conn = mongoose.createConnection(dbURL);
   await conn
     .model('ProductImage', modelSchemas.productImageSchema)
-    .create(data);
+    .create(data.sampleData);
 
   conn.close(() => {
     console.log('closing connection');
   });
 };
-
-exports.uploadImage = uploadImage;
